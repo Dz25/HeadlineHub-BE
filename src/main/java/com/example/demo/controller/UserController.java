26de2ee20878a5c51fe54f6220d9903ae30bb369
jaydipkmulani <jaydipkmulani@gmail.com>
2023-02-20 21:23:18 -0800

    this is updated file

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,9 +61,9 @@ public class UserController {
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-	
-	@GetMapping("/{id}/articales")
-	public ResponseEntity<User> getSaveUsers(@PathVariable Long id){
+	//this method is to access saved post by user to read later 
+	@GetMapping("/{id}/articles")
+	public ResponseEntity<User> getSavedUsers(@PathVariable Long id){
 		try {
 		  Optional<User> user1= userRepo.findById(id);
 		  if(user1.isPresent()){
@@ -70,22 +71,40 @@ public class UserController {
 			  newUser.getArticles();
 			  return new ResponseEntity<>(newUser, HttpStatus.OK);
 		  }
-		  return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		  return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		  
 		} catch (Exception e) {
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-	@PostMapping("/{id}/savearticales")
+	//this method is  save post by user to read later
+	@PostMapping("/{id}/articles")
 	public ResponseEntity<User> savePost(@PathVariable Long id , @Validated @RequestBody  Article article){
 		try {
 			  Optional<User> user1= userRepo.findById(id);
 			  if(user1.isPresent()){
 				  User newUser= user1.get();
 				  newUser.addArticle(article);
-				  return new ResponseEntity<>(newUser, HttpStatus.OK);
+				  return new ResponseEntity<>(newUser, HttpStatus.CREATED);
 			  }
-			  return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+			  return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			  
+			} catch (Exception e) {
+				return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+		
+	}
+	//this method is to delete saved post by user to read later
+	@DeleteMapping("/{id}/articles")
+	public ResponseEntity<HttpStatus>deletSavedPost(@PathVariable Long id , @Validated @RequestBody  Article article){
+		try {
+			  Optional<User> user1= userRepo.findById(id);
+			  if(user1.isPresent()){
+				  User newUser= user1.get();
+				  newUser.removeArticle(article);
+				  return new ResponseEntity<>( HttpStatus.OK);
+			  }
+			  return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 			  
 			} catch (Exception e) {
 				return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
